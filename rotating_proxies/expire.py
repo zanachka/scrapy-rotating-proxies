2c85896ac4dd5cb02b0ffa58415b78a7cc047217
Arthur <48801049+devfox-se@users.noreply.github.com>
2025-01-29 23:19:17 +0400

    Minor styling

--- rotating_proxies/expire.py
+++ rotating_proxies/expire.py
@@ -166,7 +166,7 @@ def exp_backoff(attempt, cap=3600, base=300):
     """ Exponential backoff time """
     # this is a numerically stable version of
     # min(cap, base * 2 ** attempt)
-    max_attempts = math.log2(cap / base)
+    max_attempts = math.log(cap / base, 2)
     if attempt <= max_attempts:
         return base * 2 ** attempt
     return cap
